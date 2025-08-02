bb14204d6bddb95ee71728db0a33dfbb0b67fb08
Bernardo <bernardo.csantanna@gmail.com>
2025-08-02 16:46:39 -0300

    Adicionado ContatoTelefones  ContatoEmails

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -3,20 +3,56 @@ Script de criação do esquema relacional
 */
 
 -- #############################################################################
--- # SEÇÃO 1: ENTIDADES INDEPENDENTES E DE APOIO
--- # Tabelas que não possuem (ou possuem poucas) dependências de outras.
+-- # Entidade Pessoa
 -- #############################################################################
 
 CREATE TABLE Pessoa (
     CPF VARCHAR(11) PRIMARY KEY,
     Nome VARCHAR(255) NOT NULL,
-    PRIMARY KEY (CPF)
 );
+
+CREATE TABLE ContatoTelefones (
+    CPF VARCHAR(11) NOT NULL,
+    Telefone VARCHAR(20) NOT NULL,
+    CONSTRAINT PK_ContatoTelefones PRIMARY KEY (CPF, Telefone),
+    FOREIGN KEY (CPF) REFERENCES Pessoa(CPF) ON DELETE CASCADE
+);
+
+CREATE TABLE ContatoEmails (
+    CPF VARCHAR(11) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    CONSTRAINT PK_ContatoEmails PRIMARY KEY (CPF, Email),
+    FOREIGN KEY (CPF) REFERENCES Pessoa(CPF) ON DELETE CASCADE
+);
+
+-- #############################################################################
+-- # Entidade PessoaLGBT
+-- #############################################################################
 
 CREATE TABLE PessoaLGBT (
     CPF VARCHAR(11) PRIMARY KEY,
     NomeSocial VARCHAR(255) NOT NULL,
-    PRIMARY KEY (CPF),
     FOREIGN KEY (CPF) REFERENCES Pessoa(CPF) ON DELETE CASCADE
 );
 
+-- #############################################################################
+-- # Entidade Servidor
+-- #############################################################################
+
+CREATE TABLE Servidor (
+    CPF VARCHAR(11) PRIMARY KEY,
+    TipoDeContrato VARCHAR(100) NOT NULL,
+    CodigoDepartamento INT NOT NULL,
+    FOREIGN KEY (CPF) REFERENCES Pessoa(CPF) ON DELETE RESTRICT,
+    FOREIGN KEY (CodigoDepartamento) REFERENCES Departamento(Codigo) ON DELETE RESTRICT
+);
+
+-- #############################################################################
+-- # Entidade Aluno
+-- #############################################################################
+
+CREATE TABLE Aluno (
+    CPF VARCHAR(11) PRIMARY KEY,
+    Matricula VARCHAR(9) NOT NULL,
+    
+);
