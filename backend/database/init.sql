1182f54ecbba5369c40f2cdf87059a3c18a2a4d0
Bernardo <bernardo.csantanna@gmail.com>
2025-08-05 23:03:41 -0300

    add new entities and attributes for inclusion, reports, schedules, and various categories in the database schema

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -116,4 +116,180 @@ CREATE TABLE BolsistaProducao(
     FOREIGN KEY (ID_BOLSISTA) REFERENCES Bolsista(ID_BOLSISTA) ON DELETE RESTRICT
 );
 
+-- Entidade Bolsista de Inclusão
+CREATE TABLE Bolsista_Inclusao(
+    ID_Bolsista INT PRIMARY KEY NOT NULL,
+    FOREIGN KEY (ID_Bolsista) REFERENCES Bolsista(ID_Membro) ON DELETE RESTRICT
+);
+
+-- Atributo Relatórios Multivalorado do Bolsista de Inclusão
+CREATE TABLE Relatorios(
+    ID_BOLSISTA INT NOT NULL,
+    DataReferente DATE NOT NULL,
+    Relatorios_Semanais VARCHAR(1000),
+    FOREIGN KEY (ID_BOLSISTA) REFERENCES Bolsista_Inclusao(ID_BOLSISTA) ON DELETE RESTRICT
+    CONSTRAINT ID_RELATORIO PRIMARY KEY (ID_BOLSISTA, DataReferente),
+);
+
+-- Atributo Horários Multivalorado do Bolsista de Inclusão
+CREATE TABLE Horarios(
+    ID_HORARIOS INT PRIMARY KEY,
+    Horarios_Monitoria DATE,
+    FOREIGN KEY (ID_HORARIOS) REFERENCES Bolsista_Producao(ID_BOLSISTA) ON DELETE RESTRICT
+);
+
+-- Entidade Estagiário
+CREATE TABLE Estagiário{
+	ID_ESTAGIARIO INT PRIMARY KEY NOT NULL;
+	Salario MONEY NOT NULL,
+	CargaHoraria INT, 
+	FOREIGN KEY (ID_ESTAGIARIO) REFERENCES MembroDeEqyuipe (ID_MEMBRO) ON DELETE RESTRICT 
+};
+
+-- Entidade PCD
+CREATE TABLE PCD {
+	ID_PCD INT PRIMARY KEY AUTO_INCREMENT, 
+};
+
+-- Entidade Deficiência
+CREATE TABLE Deficiencia(
+    ID_DEFICIENCIA INT PRIMARY KEY,
+    Categoria VARCHAR(50) NOT NULL,
+    FOREIGN KEY (ID_DEFICIENCIA) REFERENCES Bolsista(ID_Membro) ON DELETE RESTRICT
+);
+
+-- Entidade Ação
+CREATE TABLE Acao(
+    ID_ACAO INT PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL
+    Descricao VARCHAR(500),
+);
+
+-- ################ NÃO REVISADO ################
+-- Entidade Tecnologia
+CREATE TABLE Tecnologia(
+    ID_TECNOLOGIA INT PRIMARY KEY,
+    Modelo VARCHAR(50) NOT NULL,
+    N_Serie INT NOT NULL,
+    Localizacao VARCHAR(100),
+    ID_Categoria INT NOT NULL,
+
+    FOREIGN KEY (ID_Categoria) REFERENCES Categoria_Tecnologia(ID_CATEGORIA) ON DELETE RESTRICT
+);
+
+-- Entidade Categoria Tecnologia
+CREATE TABLE Categoria_Tecnologia(
+    ID_CATEGORIA INT PRIMARY KEY,
+    Tipo_Categoria VARCHAR(50) NOT NULL
+);
+
+-- Entidade Categoria Material
+CREATE TABLE CategoriaMaterial(
+    ID_CATEGORIA INT PRIMARY KEY AUTO_INCREMENT,
+    TipoMaterial VARCHAR(100) NOT NULL,
+);
+
+-- Entidade Material Acessível
+CREATE TABLE MaterialAcessivel(
+    ID_MATERIAL INT PRIMARY KEY AUTO_INCREMENT,
+    Titulo VARCHAR(255) NOT NULL,
+    Status VARCHAR(255) NOT NULL,
+    Localizacao VARCHAR(255) NOT NULL,
+    ID_CATEGORIA INT NOT NULL,
+    ID_BOLSISTA INT,
+    FOREIGN KEY (ID_CATEGORIA) REFERENCES CategoriaMaterial(ID_CATEGORIA) ON DELETE RESTRICT,
+    FOREIGN KEY (ID_BOLSISTA) REFERENCES Bolsista_Producao(ID_BOLSISTA) ON DELETE RESTRICT,
+);
+
+-- Entidade Setor
+CREATE TABLE Departamento/Setor {
+	CODIGO INT PRIMARY KEY,
+	Nome VARCHAR(60) NOT NULL,
+	Localizacao VARCHAR(100) NOT NULL,
+	Telefone VARCHAR(20) NOT NULL,
+	E-mail VARCHAR (100) NOT NULL,
+};
+
+-- Entidade Cargo
+CREATE TABLE Cargo {
+	ID INT PRIMARY KEY, 
+	Nome VARCHAR(60) NOT NULL,
+};
+
+-- Entidade Curso
+CREATE TABLE Curso {
+	CODIGO INT PRIMARY KEY, 
+	Nome VARCHAR(50) NOT NULL, 
+Modalidade VARCHAR(20) NOT NULL,
+NivelDeFormacao VARCHAR(20) NOT NULL,
+}; 
+
+-- Atributo Funções Multivalorado de Cargo
+CREATE TABLE TabelaFuncoes{
+	ID_CARGO INT NOT NULL,
+    Funcao VARCHAR(255), 
+    CONSTRAINT ID_PERIODO PRIMARY KEY (ID_CARGO, Funcao),
+};
+
+-- Relacionamento Matriculado Em
+CREATE TABLE MatriculadoEm {
+	CPF VARCHAR(11) NOT NULL,
+	Codigo INT NOT NULL, 
+	Situacao VARCHAR (50),
+	DataInicio DATE,
+	DataFim DATE, 
+	CONSTRAINT ID_Matriculado PRIMARY KEY (CPF, Codigo),
+	FOREIGN KEY (CPF) REFERENCES Aluno(CPF) ON DELETE RESTRICT
+	FOREIGN KEY (Codigo) REFERENCES Curso(Codigo) ON DELETE RESTRICT
+};
+
+-- Relacionamento Empréstimo Material
+CREATE TABLE EmprestimoMaterial (
+    IDPCD INT,
+    IDMaterial INT,
+    DataEmprestimo DATE,
+    DataDevolucao DATE,
+    DevolucaoEstimada DATE,
+    FOREIGN KEY (IDPCD) REFERENCES PCD(ID),
+    FOREIGN KEY (IDMaterial) REFERENCES TecnologiaEmprestavel(ID)
+);
+
+
+-- Relacionamento Material Disponibilizado
+CREATE TABLE MaterialDisponibilizado(
+    ID_PCD INT NOT NULL,
+    ID_MATERIAL INT NOT NULL,
+    FOREIGN KEY (ID_PCD) REFERENCES PCD(ID_PCD) ON DELETE RESTRICT,
+    FOREIGN KEY (ID_MATERIAL) REFERENCES MaterialAcessivel(ID_MATERIAL),
+    CONSTRAINT ID_DISPONIBILIZADO PRIMARY KEY (ID_PCD, ID_MATERIAL)
+);
+
+-- Entidade Deficiência
+CREATE TABLE DadosDeficienciaPCD (
+    IDPCD INT NOT NULL,
+    IDDeficiencia INT NOT NULL,
+    Grau VARCHAR(100),
+    Observacoes TEXT,
+
+    PRIMARY KEY (IDPCD, IDDeficiencia),
+
+    FOREIGN KEY (IDPCD) REFERENCES PCD(ID_PCD) ON DELETE RESTRICT,
+    FOREIGN KEY (IDDeficiencia) REFERENCES Deficiencia(ID_DEFICIENCIA) ON DELETE RESTRICT
+);
+
+-- Relacionamento Presta Assistência
+CREATE TABLE PrestaAssistencia (
+    IDPCD INT NOT NULL,
+    IDAcao INT NOT NULL,
+    IDMembroCain INT NOT NULL,
+    DataInicio DATE NOT NULL,
+    DataFim DATE,
+
+    PRIMARY KEY (IDPCD, IDAcao, IDMembroCain, DataInicio),
+
+    FOREIGN KEY (IDPCD) REFERENCES PCD(ID_PCD) ON DELETE RESTRICT,
+    FOREIGN KEY (IDAcao) REFERENCES Acao(ID_ACAO) ON DELETE RESTRICT,
+    FOREIGN KEY (IDMembroCain) REFERENCES MembroDaEquipe(ID_MEMBRO_DA_EQUIPE) ON DELETE RESTRICT
+);
+
 
